d3f91895cf0a08f515a02dc1a89efd423671abcb
alleriali <44931314+alleriali@users.noreply.github.com>
2019-03-18 22:47:21 +0100

    Add files via upload

---
+++ pdf2txt.py
@@ -0,0 +1,56 @@
+import sys
+import io
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice, TagExtractor
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.cmapdb import CMapDB
+from pdfminer.layout import LAParams
+from pdfminer.image import ImageWriter
+
+# main
+def pdf2txt(file_path):
+    # debug option
+    debug = 0
+    # input option
+    password = ''
+    pagenos = set()
+    maxpages = 0
+    # output option
+    outfile = None
+    outtype = None
+    imagewriter = None
+    rotation = 0
+    stripcontrol = False
+    layoutmode = 'normal'
+    codec = 'utf-8'
+    pageno = 1
+    scale = 1
+    caching = True
+    showpageno = True
+    laparams = LAParams()
+    #
+    PDFDocument.debug = debug
+    PDFParser.debug = debug
+    CMapDB.debug = debug
+    PDFPageInterpreter.debug = debug
+    #
+    rsrcmgr = PDFResourceManager(caching=caching)
+    outfp = io.StringIO()
+    device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams,
+                            imagewriter=imagewriter)
+    for fname in [file_path]:
+        fp = open(fname, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(fp, pagenos,
+                                      maxpages=maxpages, password=password,
+                                      caching=caching, check_extractable=True):
+            page.rotate = (page.rotate+rotation) % 360
+            interpreter.process_page(page)
+        fp.close()
+    device.close()
+    result = outfp.getvalue()
+    outfp.close()
+    return result
